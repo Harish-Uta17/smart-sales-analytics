70a7424e2060af774406f36eb7827be711b5d8a2
Harish-Uta17 <utaharish78@gmail.com>
2026-01-11 15:10:46 +0530

    Final cloud-ready smart sales Analytics app

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,12 +1,32 @@
-from sqlalchemy import create_engine # type: ignore
 import pandas as pd
+import streamlit as st
+from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://postgres:9913933238@localhost/demo")
+engine = create_engine(st.secrets["DB_URL"])
 
-sales = pd.read_sql("SELECT * FROM sales", engine)
-products = pd.read_sql("SELECT * FROM products", engine)
-customers = pd.read_sql("SELECT * FROM customers", engine)
+customers = pd.DataFrame({
+    "customer_id":[1,2,3],
+    "name":["Rahul","Anita","Suresh"],
+    "city":["Bangalore","Hyderabad","Chennai"],
+    "age":[22,24,27]
+})
 
-print(sales.head())
-print(products.head())
-print(customers.head())
+products = pd.DataFrame({
+    "product_id":[101,102,103],
+    "product_name":["Laptop","Mobile","Headphones"],
+    "categoty":["Electronics","Electronics","Accessories"],
+    "price":[50000,30000,3000]
+})
+
+sales = pd.DataFrame({
+    "customer_id":[1,2,3,1,2],
+    "product_id":[101,102,103,102,101],
+    "quantity":[1,2,3,1,2],
+    "sale_date":["2024-01-10","2024-01-12","2024-02-05","2024-02-20","2024-03-01"]
+})
+
+customers.to_sql("customers",engine,if_exists="append",index=False)
+products.to_sql("products",engine,if_exists="append",index=False)
+sales.to_sql("sales",engine,if_exists="append",index=False)
+
+print("DATA INSERTED INTO SUPABASE SUCCESSFULLY")
